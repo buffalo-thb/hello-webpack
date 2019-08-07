688b4959012658eacebac4e6987131d29cd2d154
thb <justdoit_thb@163.com>
2019-08-08 00:43:35 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// import printMe from './print.js'
+// import './style.css';
+import { cube } from './math.js';
+
+function component() {
+  var element = document.createElement('pre');
+
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
+
+  return element;
+}
+
+let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
+document.body.appendChild(element);
---
+++ src/math.js
@@ -0,0 +1,9 @@
+export function square(x) {
+  console.log('I am in square')
+  return x * x;
+}
+
+export function cube(x) {
+  console.log('I am in cube')
+  return x * x * x;
+}
