2c99af0a7fc44cc500028c93eb48df4c1a666d78
tanghaibo <tanghaibo@58ganji.com>
2019-08-12 22:35:20 +0800

    save

---
+++ dist/precache-manifest.0430558982c316a5e1b5314678f2b4de.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d6f96ffaf85d96e58c",
+    "url": "app.bundle.js"
+  },
+  {
+    "revision": "4e051cefff5301973505fa2026506a82",
+    "url": "index.html"
+  },
+  {
+    "revision": "24b6eb6a7aa63e6359dc",
+    "url": "print.bundle.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
-function component() {
-  var element = document.createElement('div');
-  var button = document.createElement('button');
-  var br = document.createElement('br');
-
-  button.innerHTML = 'Click me and look at the console!';
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.appendChild(br);
-  element.appendChild(button);
-
-  // Note that because a network request is involved, some indication
-  // of loading would need to be shown in a production-level site/app.
-  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
-    console.log(module)
-    var print = module.default;
-
-    print();
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
   });
-
-  return element;
 }
-
- document.body.appendChild(component());
