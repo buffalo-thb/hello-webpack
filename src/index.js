0bb92b95c4b95b068de10e5aa2aaf6289dd9753d
tanghaibo <tanghaibo@58ganji.com>
2019-08-13 09:26:24 +0800

    save

--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
 import _ from 'lodash';
+import numRef from './ref.json'
 
-function component() {
-  var element = document.createElement('div');
-  var button = document.createElement('button');
-  var br = document.createElement('br');
-
-  button.innerHTML = 'Click me and look at the console!';
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.appendChild(br);
-  element.appendChild(button);
-
-  // Note that because a network request is involved, some indication
-  // of loading would need to be shown in a production-level site/app.
-  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
-    console.log(module)
-    var print = module.default;
-
-    print();
-  });
-
-  return element;
+export function numToWord (num) {
+  return _.reduce(numRef, (accum, ref) => {
+    return ref.num === num ? ref.word : accum;
+  }, '');
 }
 
- document.body.appendChild(component());
+export function wordToNum (word) {
+  return _.reduce(numRef, (accum, ref) => {
+    return ref.word === word && word.toLowerCase() ? ref.num : accum;
+  }, -1);
+}
