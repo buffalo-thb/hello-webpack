02c900b3a4d097d1b413038a672e6ff405b4d6cd
tanghaibo <tanghaibo@58ganji.com>
2019-10-09 17:54:13 +0800

    complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+
+
 function component() {
   var element = document.createElement('div');
 
   element.innerHTML = join(['Hello', 'webpack'], ' ');
 
+  // Assume we are in the context of `window`
+  this.alert('Hmmm, this probably isn\'t a great idea...')
+
   return element;
 }
+
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    index: './src/index.js',
-    polyfills: './src/polyfills.js'
+    polyfills: './src/polyfills.js',
+    index: './src/index.js'
   },
   output: {
     filename: '[name].bundle.js',
@@ -15,10 +15,6 @@ module.exports = {
       {
         test: require.resolve('./src/index.js'),
         use: 'imports-loader?this=>window'
-      },
-      {
-        test: require.resolve('./src/globals.js'),
-        use: 'exports-loader?file,parse=helpers.parse'
       }
     ]
   },
